52777e0344bb8d1a758bf83df1c478aad884ce0b
rosa10 <38325869+rosa10@users.noreply.github.com>
2018-05-04 05:30:14 +0800

    Update v_form.php

--- application/views/v_form.php
+++ application/views/v_form.php
@@ -1,25 +1,19 @@
-<?php
-
-class Form extends CI_Controller{
-
-	function __construct(){
-		parent::__construct();
-		$this->load->library('form_validation');
-	}
-
-	function index(){
-		$this->load->view('v_form');
-	}
-
-	function aksi(){
-		$this->form_validation->set_rules('nama','Nama','required');
-		$this->form_validation->set_rules('email','Email','required');
-		$this->form_validation->set_rules('konfir_email','Konfirmasi Email','required');
-
-		if($this->form_validation->run() != false){
-			echo "Form validation oke";
-		}else{
-			$this->load->view('v_form');
-		}
-	}
-}
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat form validation dengan Codeigniter | MalasNgoding.com</title>
+</head>
+<body>
+	<h1>Membuat Form Validation dengan CodeIgniter</h1>
+	<?php echo validation_errors(); ?>
+	<?php echo form_open('form/aksi'); ?>
+		<label>Nama</label><br/>
+		<input type="text" name="nama"><br/>
+		<label>Email</label><br/>
+		<input type="text" name="email"><br/>
+		<label>Konfirmasi Email</label><br/>
+		<input type="text" name="konfir_email"><br/>
+		<input type="submit" value="Simpan">
+	</form>
+</body>
+</html>
